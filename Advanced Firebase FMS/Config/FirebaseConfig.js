afc3ef2f28ab197bd8e1435e827e1488bbe5401d
Mihir Kumawat <kumawatmihir21@gmail.com>
2025-03-04 13:30:56 +0530

    adding advanced react firebase fms

---
+++ Advanced Firebase FMS/Config/FirebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyABtr6asBwB4n4oldRmD90ZtWE680RUvhU',
+  authDomain: "firewall-fms.firebaseapp.com",
+  projectId: "firewall-fms",
+  storageBucket: "firewall-fms.firebasestorage.app",
+  messagingSenderId: "973516556647",
+  appId: "1:973516556647:web:504c67e1930d368af7c7c2",
+  measurementId: "G-66XFBWBSKG"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
